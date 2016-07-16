ebb184f91b40b02fbe006052436aed0f578fe79e
Austin Spafford <austin.spafford@gmail.com>
2016-07-15 20:08:19 -0700

    Adding ClickCausesProgressChange

---
+++ Assets/GeomHunt/Scripts/Clicking/ClickCausesProgressChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ClickTarget))] // The click-target is what sends us messages.
+public class ClickCausesProgressChange : MonoBehaviour
+{
+	public string ProgressName = "";
+	public float ProgressValue = 0.0f;
+
+	public void OnClickBegin(
+		ClickSource clickSource)
+	{
+		var progressStorage = ProgressStorage.LocalPlayerProgressStorage;
+		
+		progressStorage.SetProgressValueAsFloat(ProgressName, ProgressValue);
+	}
+}
--- Assets/GeomHunt/Scripts/Player/ProgressStorage.cs
+++ Assets/GeomHunt/Scripts/Player/ProgressStorage.cs
@@ -20,32 +20,19 @@ public delegate void ProgressChangedEventHandler(ProgressChangedEventArgs eventA
 
 public class ProgressStorage : MonoBehaviour
 {
+	public static ProgressStorage LocalPlayerProgressStorage
+	{
+		get
+		{
+			UpdateCachedLocalPlayerStorageReference();
+
+			return cachedLocalPlayerProgressStorage;
+		}
+	}
+
 	public event ProgressChangedEventHandler ProgressChanged;
 
-	public static ProgressStorage GetLocalPlayerProgressStorage()
-	{
-		// If we need initialize or reinitialize the cache (eg. after a total scene-wipe).
-		if (cachedLocalPlayerProgressStorage == null)
-		{
-			ProgressStorage[] progressStorages = FindObjectsOfType<ProgressStorage>();
-
-			if (progressStorages.Length == 0)
-			{
-				throw new System.InvalidOperationException(
-					"There are no active ProgressStorage components, thus there's no progress storage for the local player.");
-			}
-
-			if (progressStorages.Length > 1)
-			{
-				throw new System.InvalidOperationException(
-					"There are multiple active ProgressStorage components, thus it's ambiguous as to which one is for the local player. This likely means ProgressStorage itself needs to be further iterated upon to remove the ambiguity.");
-			}
-
-			cachedLocalPlayerProgressStorage = progressStorages[0];
-		}
-
-		return cachedLocalPlayerProgressStorage;
-	}
+	public bool DebugEnabled = false;
 
 	public bool GetProgressValueAsBool(
 		string progressName)
@@ -113,11 +100,41 @@ public class ProgressStorage : MonoBehaviour
 	private static ProgressStorage cachedLocalPlayerProgressStorage = null;
 
 	private Dictionary<string, ProgressEntry> progressEntries = new Dictionary<string, ProgressEntry>();
+	
+	private static void UpdateCachedLocalPlayerStorageReference()
+	{
+		// If we need initialize or reinitialize the cache (eg. after a total scene-wipe).
+		if (cachedLocalPlayerProgressStorage == null)
+		{
+			ProgressStorage[] progressStorages = FindObjectsOfType<ProgressStorage>();
+
+			if (progressStorages.Length == 0)
+			{
+				throw new System.InvalidOperationException(
+					"There are no active ProgressStorage components, thus there's no progress storage for the local player.");
+			}
+
+			if (progressStorages.Length > 1)
+			{
+				throw new System.InvalidOperationException(
+					"There are multiple active ProgressStorage components, thus it's ambiguous as to which one is for the local player. This likely means ProgressStorage itself needs to be further iterated upon to remove the ambiguity.");
+			}
+
+			cachedLocalPlayerProgressStorage = progressStorages[0];
+		}
+	}
 
 	private ProgressEntry GetOrCreateProgressEntry(
 		string progressName)
 	{
 		ProgressEntry result = null;
+
+		if (string.IsNullOrEmpty(progressName))
+		{
+			throw new System.ArgumentNullException(
+				progressName, 
+				"Progress names must be non-empty strings.");
+		}
 
 		if (progressEntries.TryGetValue(progressName, out result))
 		{
@@ -153,13 +170,22 @@ public class ProgressStorage : MonoBehaviour
 			eventArgs.OldValueAsFloat = progressEntry.ProgressValueAsFloat;
 			eventArgs.OldValueAsInt = progressEntry.ProgressValueAsInt;
 
-			// Now that the old values have been recorded, actualize the change.
+			// Now that the old values have been recorded, actualize the progress-change.
 			progressEntry.ProgressValueStorage = newValue;			
 
 			eventArgs.NewValueAsBool = progressEntry.ProgressValueAsBool;
 			eventArgs.NewValueAsFloat = progressEntry.ProgressValueAsFloat;
 			eventArgs.NewValueAsInt = progressEntry.ProgressValueAsInt;
 			
+			if (DebugEnabled)
+			{
+				Debug.LogFormat(
+					"Changing progress [{0}] from [{1:F2}] to [{2:F2}].",
+					eventArgs.ProgressName,
+					eventArgs.OldValueAsFloat,
+					eventArgs.NewValueAsFloat);
+			}
+
 			if (ProgressChanged != null)
 			{
 				ProgressChanged(eventArgs);
